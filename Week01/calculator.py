e608643e95df83714327f50ce407c772dad827c3
김수혁 <shsh6216@gmail.com>
2026-08-16 20:41:55 +0900

    feat: hello 파이썬 파일 추가

---
+++ Week01/calculator.py
@@ -0,0 +1,11 @@
+num1 = int(input('첫번째 숫자 : '))
+num2 = int(input('두번째 숫자 : '))
+
+print('더하기 : ', num1 + num2)
+print('빼기 : ', num1 - num2)
+print('곱하기 : ', num1 * num2)
+
+if num2 == 0:
+    print('나누기 : 0으로 나눌 수 없습니다')
+else : 
+    print('나누기 : ', num1 / num2)
