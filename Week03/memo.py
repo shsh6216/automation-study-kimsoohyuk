a26b5563c692fac331fcfd9ce4727008bc5e63ef
김수혁 <shsh6216@gmail.com>
2026-09-13 17:24:04 +0900

    memo.py 수정

--- Week03/memo.py
+++ Week03/memo.py
@@ -3,12 +3,22 @@ import sys
 
 # 메모 저장
 def save_memos(memos):
-    with open("memos.json", "w") as file:
-        json.dump(memos, file, indent=4)
+    with open("memos.json", "w", encoding="utf-8") as file:
+        json.dump(memos, file, ensure_ascii=False, indent=4)
 
+def load_memos():
+    try:
+        with open("memos.json", "r", encoding="utf-8") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        print("추가한 메모 없음")
+        return []
+    except json.JSONDecodeError:
+        print("JSON 형식이 잘못됨")
+        return []
 #======================================
 if __name__ == "__main__":
-    memos = []
+    memos = load_memos()
     
     memo = input("메모 입력 : ")
     memos.append(memo)
@@ -16,12 +26,17 @@ if __name__ == "__main__":
     print("메모 추가 완료")
 
 # 전체 메모 조회
-    for memo in memos:
-        print(memo)
+    print("입력된 메모 : ",memos)
 
 # 특정 내용 검색
     search = input("검색할 내용 : ")
 
+    found = False
+
     for memo in memos:
         if search in memo:
-            print(memo)
+            print("검색한 내용과 일치하는 메모 : ", memo)
+            found = True
+
+    if not found:
+        print("검색 내용 없음")
